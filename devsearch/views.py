275662d65c2ff625f796b310ca23242863b3474d
Ahmet Hakan Beşel <ahmethakan.besel@gmail.com>
2022-02-22 15:16:56 +0300

    Handle detail page

--- devsearch/views.py
+++ devsearch/views.py
@@ -1,10 +1,13 @@
 import json
 
+from django.db.models import Sum
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
-from .models import Developer
+from .models import Developer, Repository
 from .forms import SearchForm
 import urllib.parse
+
+from .services import search_on_github, save_user, get_user
 
 
 def index(request):
@@ -30,10 +33,10 @@ def search(request):
 
 def result(request, keyword):
     # get data from github
-    # developer_data = search_on_github(keyword, 1)
+    developer_data = search_on_github(keyword, 1)
     # read json file
-    with open('./devsearch/result.json') as json_file:
-        developer_data = json.load(json_file)
+    # with open('./devsearch/result.json') as json_file:
+    #    developer_data = json.load(json_file)
     context = {
         "keyword": keyword,
         "total_count": developer_data['total_count'],
@@ -44,7 +47,29 @@ def result(request, keyword):
 
 def detail(request, username):
     try:
-        question = Developer.objects.get(username=username)
+        developer = Developer.objects.get(username=username)
+        repositories = Repository.objects.filter(owner=developer)
+        developer.stars = repositories.aggregate(Sum('stars'))['stars__sum']
+        developer.forks = repositories.aggregate(Sum('forks'))['forks__sum']
+        languages = repositories.values('language').exclude(language=None).distinct()
+        repositories = repositories.order_by('-stars')
     except Developer.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'devsearch/profile.html', {'question': question})
+        user = get_user(username)
+        if 'login' in user:
+            save_user(user)
+            developer = Developer.objects.get(username=username)
+            repositories = Repository.objects.filter(owner=developer)
+            developer.stars = repositories.aggregate(Sum('stars'))['stars__sum']
+            developer.forks = repositories.aggregate(Sum('forks'))['forks__sum']
+            languages = repositories.values('language').exclude(language=None).distinct()
+            repositories = repositories.order_by('-stars')
+            return render(request, 'devsearch/profile.html',
+                          {'developer': developer, 'repositories': repositories, 'languages': languages})
+        else:
+            raise Http404("GitHub account does not exist")
+    return render(request, 'devsearch/profile.html',
+                  {'developer': developer, 'repositories': repositories, 'languages': languages})
+
+
+def not_found(request, exception):
+    return render(request, 'devsearch/404.html')
