0d899db683dd90591f57af1361aa6b63f4051891
Ahmet Hakan Beşel <ahmethakan.besel@gmail.com>
2022-02-22 15:15:50 +0300

    Add custom 404 handler

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+handler404 = 'devsearch.views.not_found'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('search', views.search, name='search'),
