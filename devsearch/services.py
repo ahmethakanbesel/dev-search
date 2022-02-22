30edce0e6c6c0376e1dc36e63a9a3b43b114e73f
Ahmet Hakan Beşel <ahmethakan.besel@gmail.com>
2022-02-22 15:16:40 +0300

    Add cache service for search results

--- devsearch/services.py
+++ devsearch/services.py
@@ -1,21 +1,53 @@
+import json
+import os
 from datetime import datetime, timedelta
 import urllib.parse
+from hashlib import sha256
+from os import path
 
 import requests
 
 from devsearch.models import Developer, Repository, RepositoryTopic
 
 
-def search_on_github(keyword: str, page: int, per_page=40):
+def cache_valid(digest: str, days=1):
+    cache_path = 'devsearch/cache/' + digest + '.json'
+    if path.exists(cache_path):
+        import datetime
+        today = datetime.datetime.today()
+        modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(cache_path))
+        duration = today - modified_date
+        if duration.days < days:
+            return True
+    return False
+
+
+def search_on_github(keyword: str, page: int, per_page=20):
     params = {'q': keyword, 'page': page, 'per_page': per_page}
-    url = "https://api.github.com/search/users?" + urllib.parse.urlencode(params)
-    response = requests.request("GET", url)
-    data = response.json()
-    return data
+    # check cache.json exists
+    digest = sha256(json.dumps(params, sort_keys=True).encode('utf8')).hexdigest()
+    if cache_valid(digest):
+        with open('devsearch/cache/' + digest + '.json') as f:
+            data = json.load(f)
+            return data
+    else:
+        url = "https://api.github.com/search/users?" + urllib.parse.urlencode(params)
+        response = requests.request("GET", url)
+        data = response.json()
+        i = 0
+        if 'items' in data:
+            for user in data['items']:
+                data['items'][i] = get_user(user['login'])
+                if 'login' in user:
+                    save_user(data['items'][i])
+                i += 1
+            with open('devsearch/cache/' + digest + '.json', 'w') as f:
+                json.dump(data, f)
+        return data
 
 
 def get_user_data(username: str):
-    url = "https://api.github.com/search/users/" + username
+    url = "https://api.github.com/users/" + username
     response = requests.request("GET", url)
     data = response.json()
     return data
@@ -36,8 +68,9 @@ def get_user(username: str):
 
 
 def save_user(developer):
-    if not Developer.objects.filter(username=developer['login']).exists() or Developer.objects.get(
-            username=developer['login']).updated_at < datetime.now() - datetime.timedelta(days=30):
+    time_threshold = datetime.now() - timedelta(days=30)
+    if not Developer.objects.filter(username=developer['login']).exists() or Developer.objects.filter(
+            username=developer['login'], updated_at__lt=time_threshold).exists():
         # save to database
         dev = Developer.objects.create(
             name=developer['name'],
@@ -55,7 +88,7 @@ def save_user(developer):
         for repo in developer['repositories']:
             if not Repository.objects.filter(name=repo['name'], owner=dev.id).exists():
                 Repository.objects.create(
-                    owner=dev.id,
+                    owner=dev,
                     name=repo['name'],
                     description=repo['description'],
                     stars=repo['stargazers_count'],
@@ -64,15 +97,16 @@ def save_user(developer):
                     has_wiki=repo['has_wiki'],
                     has_issues=repo['has_issues'],
                     has_projects=repo['has_projects'],
-                    license=repo['license']['name'],
+                    license=repo['license']['name'] if repo['license'] else None,
                     last_push=repo['pushed_at']
                 )
 
 
 def save_to_database(developer_data):
+    time_threshold = datetime.now() - timedelta(days=30)
     for developer in developer_data:
         if not Developer.objects.filter(username=developer['login']).exists() or Developer.objects.get(
-                username=developer['login']).updated_at < datetime.now() - datetime.timedelta(days=30):
+                username=developer['login'], updated_at__lt=time_threshold):
             # save to database
             Developer.objects.create(
                 name=developer['name'],
